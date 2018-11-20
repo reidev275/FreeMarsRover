8ebb7386c8aee9bcb188bb5f6d39418396733153
Reid Evans <reidev275@gmail.com>
2018-11-19 19:12:42 -0500

    finish right and back

--- src/FreeDsl.purs
+++ src/FreeDsl.purs
@@ -1,6 +1,6 @@
 module FreeDsl where
 
-import Prelude (type (~>), discard, pure, show, ($), (+), (-))
+import Prelude (type (~>), discard, pure, show, ($), (+), (-), identity)
 import Control.Monad.Free (Free, liftF, foldFree)
 import Effect (Effect)
 import Effect.Console (log)
@@ -8,16 +8,23 @@ import Model (Direction(..), State(..))
 
 data DslF a
   = Forward State (State -> a)
+  | Backward State (State -> a)
   | Left State (State -> a)
+  | Right State (State -> a)
 
 type Dsl = Free DslF
 
 forward :: State -> Dsl State
-forward state = liftF $ Forward state \a -> a
+forward state = liftF $ Forward state identity
+
+backward :: State -> Dsl State
+backward state = liftF $ Backward state identity
 
 left :: State -> Dsl State
-left state = liftF $ Left state \a -> a
+left state = liftF $ Left state identity
 
+right :: State -> Dsl State
+right state = liftF $ Right state identity
 
 leftImpl :: State -> State
 leftImpl (State s) =
@@ -26,6 +33,14 @@ leftImpl (State s) =
     South -> State s { direction = East }
     East  -> State s { direction = North }
     West  -> State s { direction = South }
+
+rightImpl :: State -> State
+rightImpl (State s) = 
+  case s.direction of
+    North -> State s { direction = East }
+    South -> State s { direction = West }
+    East  -> State s { direction = South }
+    West  -> State s { direction = North }
 
 forwardImpl :: State -> State
 forwardImpl (State s) = 
@@ -36,18 +51,31 @@ forwardImpl (State s) =
     West  -> State s { y = s.y - 1 }
 
 
+backwardImpl :: State -> State
+backwardImpl (State s) = 
+  case s.direction of
+    North -> State s { x = s.x - 1 }
+    South -> State s { x = s.x + 1 }
+    East  -> State s { y = s.y - 1 }
+    West  -> State s { y = s.y + 1 }
+
+display :: forall a. (State -> State) -> State -> (State -> a) -> Effect a
+display impl state next = do
+  log $ show s1
+  pure $ next s1
+  where
+    s1 = impl state
+
 interpretDsl :: Dsl ~> Effect
 interpretDsl = foldFree morph
   where
     morph :: DslF ~> Effect
-    morph (Forward state next) = do
-      log $ show s1
-      pure $ next s1
-      where
-        s1 = forwardImpl state
-    morph (Left state next) = do
-      log $ show s1
-      pure $ next s1
-      where
-        s1 = leftImpl state
+    morph (Forward state next) =
+      display forwardImpl state next
+    morph (Left state next) = 
+      display leftImpl state next
+    morph (Backward state next) =
+      display backwardImpl state next
+    morph (Right state next) = do
+      display rightImpl state next
 
--- src/Main.purs
+++ src/Main.purs
@@ -3,15 +3,19 @@ module Main where
 import Prelude (Unit, bind, discard, show, ($), (>>=))
 import Effect (Effect)
 import Effect.Console (log)
-import FreeDsl (Dsl, forward, left, interpretDsl)
+import FreeDsl (Dsl, forward, left, right, backward, interpretDsl)
 import Model (Direction(..), State(..))
 
 program :: State -> Dsl State
 program init = 
   forward init
   >>= left
+  >>= forward
+  >>= right
+  >>= forward
   >>= left
   >>= forward
+  >>= backward
 
 initial :: State 
 initial = State { x: 0, y: 0, direction: East }
